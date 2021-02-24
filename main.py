ff3b404cb715938f7bb993f9cfc0561b1f94bbc5
freememe <dreamyygamer@gmail.com>
2021-02-24 17:45:20 +0100

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ client = commands.Bot(command_prefix='!')
 
 @client.event
 async def on_ready():
-    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name='Dev:Free Memes#0001'))
+    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name='Dev: Free Memes#0001'))
 
 @client.command(name='invite',pass_context=True)
 async def invite(ctx, *argument):
